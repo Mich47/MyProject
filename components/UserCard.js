00604b24d8b0819a26345ec8674be8bc9cfba481
Dmytro Muzyka <mmitya889@gmail.com>
2023-05-29 14:51:39 +0300

    Create UserCard.js

---
+++ components/UserCard.js
@@ -0,0 +1,73 @@
+import { View, Text, Image, StyleSheet } from "react-native";
+
+export const UserCard = ({
+  avatar = null,
+  login = "Natali Romanova",
+  email = "email@example.com",
+}) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        {avatar && (
+          <Image
+            style={styles.image}
+            source={{
+              uri: avatar,
+            }}
+          />
+        )}
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.textLogin}>{login}</Text>
+        <Text style={styles.textEmail}>{email}</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    gap: 8,
+  },
+  imageContainer: {
+    position: "relative",
+    backgroundColor: "#F6F6F6",
+    width: 60,
+    height: 60,
+    borderRadius: 16,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+    borderRadius: 16,
+  },
+  imageAdd: {
+    position: "absolute",
+    right: -12,
+    bottom: 14,
+    width: 25,
+    height: 25,
+  },
+  textContainer: {
+    display: "flex",
+    gap: 2,
+  },
+  textLogin: {
+    fontFamily: "Roboto-Bold",
+    fontWeight: 700,
+    fontSize: 13,
+    lineHeight: 15,
+    color: "#212121",
+  },
+  textEmail: {
+    fontFamily: "Roboto-Regular",
+    fontSize: 11,
+    lineHeight: 13,
+    color: "rgba(33, 33, 33, 0.8)",
+  },
+});
